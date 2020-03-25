9c6a230e1e0778e9ad17e71a0da0ce4ac9f93a45
pedrosecchi67 <pedrosecchimail@gmail.com>
2020-03-25 14:44:22 -0300

    Completed README and basic repository file structure for the package

---
+++ chochoBL/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+ordir=os.getcwd()
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.getcwd())
+#module imports
+
+os.chdir(ordir)
+del ordir
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from numpy.distutils.core import Extension
+from numpy.distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+#libraries=[('toolkit', dict(sources=['toolkit.f90']))]
+extensions=[Extension(name='chochoBL.toolkit', sources=['chochoBL/toolkit.f90'], language='f90')]#, libraries='toolkit')]
+
+setup(
+    name="chochoBL",
+    version="0.0.1",
+    author="Pedro de Almeida Secchi",
+    author_email="pedrosecchimail@gmail.com",
+    description="Python based, open source finite difference boundary layer solver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pedrosecchi67/chochoBL",
+    packages=['chochoBL'],
+    #libraries=libraries,
+    ext_modules=extensions,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy', 'scipy', 'matplotlib'],
+    python_requires='>=3.6',
+)
