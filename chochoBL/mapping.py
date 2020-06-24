b40cc4a387940836e38fd77c11882269c07bcb23
pedrosecchi67 <pedrosecchimail@gmail.com>
2020-06-24 12:00:08 -0300

    Added and tested exponential mapping

--- chochoBL/mapping.py
+++ chochoBL/mapping.py
@@ -70,3 +70,26 @@ class sigma_mapping(mapping):
 
         super().__init__(lambda x: _sigma_fun_jac(x, bounds[0], bounds[1]), lambda x: special.logit((x-bounds[0])/(bounds[1]-bounds[0])), \
             inverse_verificaion=lambda x: sigma_inv_verification(x, bounds[0], bounds[1]))
+
+_identity_funjac=lambda x: (x, np.ones_like(x))
+identity_mapping=mapping(fun_jac=_identity_funjac, inverse=lambda x: x)
+
+def _exp_fun_jac(x, A):
+    f=np.exp(x)*A
+
+    return f, f
+
+def log_verification(x, tol=1e-18):
+    return np.maximum(x, tol)
+
+class exp_mapping(mapping):
+    '''
+    A conform transformation based on an exponential function
+    '''
+
+    def __init__(self, val0):
+        '''
+        Initialize conform transformation f(x)=val0.exp(x)
+        '''
+
+        super().__init__(lambda x: _exp_fun_jac(x, val0), lambda x: np.log(x/val0), inverse_verificaion=lambda x: log_verification(x/val0))
--- tests/mapping_test.py
+++ tests/mapping_test.py
@@ -1,3 +1,4 @@
+import numpy.random as rnd
 import numpy as np
 
 from chochoBL import *
@@ -21,16 +22,45 @@ def test_smap():
 
     findiff(map, np.array([2.1, 2.0, 5.0, 4.9, 0.0]))
 
-def findiff(map, val, factor=1e-7):
-    v1, j=map(val)
-    v2, _=map(val+val*factor)
+def test_ident():
+    map=identity_mapping
 
-    var_an=(val*factor)*j
+    findiff(map, 2.1)
+    findiff(map, 2.0)
+    findiff(map, 5.0)
+    findiff(map, 4.9)
+    findiff(map, 0.0)
+
+    findiff(map, np.array([2.1, 2.0, 5.0, 4.9, 0.0]))
+
+    assert map(0.5)[0]==0.5
+    assert np.all(map(np.array([0.0, 1.0]))[0]==np.array([0.0, 1.0]))
+
+def test_exp():
+    map=exp_mapping(2.0)
+
+    findiff(map, 2.1)
+    findiff(map, 2.0)
+    findiff(map, 5.0)
+    findiff(map, 4.9)
+    findiff(map, 0.0)
+
+    findiff(map, np.array([2.1, 2.0, 5.0, 4.9, 0.0]))
+
+def findiff(map, val, factor=1e-7):
+    var=np.maximum(np.abs(val)*factor, factor)
+
+    v1, j=map(val)
+    v2, _=map(val+var)
+
+    var_an=(var)*j
     var_num=(v2-v1)
 
-    print(var_an, var_num)
+    print('=====')
+    print(val, j, var_an, var_num)
+    print('=====')
 
     if type(val)==np.ndarray:
-        assert np.all(np.abs(var_an-var_num)<=np.abs(var_an)*findiff_reltol)
+        assert np.all(np.abs(var_an-var_num)<=np.abs(var_an)*findiff_reltol) or np.all(np.abs(var_an-var_num)<factor**2)
     else:
-        assert np.abs(var_an-var_num)<=np.abs(var_an)*findiff_reltol
+        assert np.abs(var_an-var_num)<=np.abs(var_an)*findiff_reltol or np.abs(var_an-var_num)<factor**2
